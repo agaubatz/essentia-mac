28d3b44aa642fa56392e1701cef4bcf6f2ba2cfd
Andrew Gaubatz <atgaubatz@gmail.com>
2019-04-27 23:05:39 -0700

    Initial Commit

---
+++ essentia-mac/essentia_mac.hpp
@@ -0,0 +1,22 @@
+//
+//  essentia_mac.hpp
+//  essentia-mac
+//
+//  Created by Andrew Gaubatz on 4/27/19.
+//  Copyright © 2019 Andrew Gaubatz. All rights reserved.
+//
+
+#ifndef essentia_mac_
+#define essentia_mac_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class essentia_mac
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
